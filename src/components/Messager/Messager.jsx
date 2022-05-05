0115bf12517d31085e13929f75712b97d559734b
Dmitrey1087751 <dmitrey.zadonskey@gmail.com>
2022-05-05 13:07:06 +0200

    25 done

--- src/components/Messager/Messager.jsx
+++ src/components/Messager/Messager.jsx
@@ -24,30 +24,29 @@ let dialogsData = [
     { id: 4, Name: "Chubaka" }
 ]
 
+let dialogs = dialogsData.map(dialog => 
+<Dialog id={dialog.id} name={dialog.Name} />
+    )
+
 let messagesData = [
-    {id: 1, text: "a;lsmlsmlkmscmck  sdsdsd  scscm"},
-    {id: 2, text: "a;lsmlsmlkmscmck   dcsdc   m"},
-    {id: 3, text: "a;lsmlsmlkmscmckm  sdcsdcsd"},
-    {id: 4, text: "a;lsmlsmlkmscmckmsdcsdsdsdsd"},
-    {id: 5, text: "a;lsmlsmlkmscmckm"}
+    { id: 1, text: "a;lsmlsmlkmscmck  sdsdsd  scscm" },
+    { id: 2, text: "a;lsmlsmlkmscmck   dcsdc   m" },
+    { id: 3, text: "a;lsmlsmlkmscmckm  sdcsdcsd" },
+    { id: 4, text: "a;lsmlsmlkmscmckmsdcsdsdsdsd" },
+    { id: 5, text: "a;lsmlsmlkmscmckm" }
 ]
+
+let messages = messagesData.map( message => <Message id={message.id} text={messages.text} />)
 
 let Messager = () => {
     return (
         <div className={style.wrapper}>
             <div className="dialogs">
-                <Dialog id={dialogsData[0].id} name={dialogsData[0].Name} />
-                <Dialog id={dialogsData[1].id} name={dialogsData[1].Name} />
-                <Dialog id={dialogsData[2].id} name={dialogsData[2].Name} />
-                <Dialog id={dialogsData[3].id} name={dialogsData[3].Name} />
+                {dialogs}
+
             </div>
             <div className="mesages">
-                <Message id={messagesData[0].id }text={messagesData[0].text} />
-                <Message id={messagesData[1].id }text={messagesData[1].text} />
-                <Message id={messagesData[2].id }text={messagesData[2].text} />
-                <Message id={messagesData[3].id }text={messagesData[3].text} />
-                <Message id={messagesData[4].id }text={messagesData[4].text} />
-                
+                { messages }
             </div>
         </div>
     )
--- src/components/Profile/Posts/Posts.jsx
+++ src/components/Profile/Posts/Posts.jsx
@@ -7,15 +7,13 @@ let postsData = [
   {id: 2, maessage: "sadjnjaiiji k cnjj   xxjiij", likesCounter: 4},
   {id: 3, maessage: "вс высфывс sadjnjaiijikcnjjxxjiij", likesCounter: 2}
 ]
+let posts = postsData.map( post => <Post id={post.id} message={post.maessage} likesCounter={post.likesCounter}/>)
 
 function Posts() {
   return (
     <div>
       <div>FORM</div>
-     <Post id={postsData[0].id} message={postsData[0].maessage} likesCounter={postsData[0].likesCounter}/>
-     <Post id={postsData[1].id} message={postsData[1].maessage} likesCounter={postsData[1].likesCounter}/>
-     <Post id={postsData[2].id} message={postsData[2].maessage} likesCounter={postsData[2].likesCounter}/>
-     
+      { posts }
     </div>
   )
 }
