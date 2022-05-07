2ac354153dafb4e6d9dbcc95023db9e29a04e7f8
Dmitrey Zadonskey <dmitrey.zadonskey@gmail.com>
2022-05-07 19:04:00 +0200

    перенес в index js лишнее испавляю

--- src/components/Messager/Messager.jsx
+++ src/components/Messager/Messager.jsx
@@ -1,38 +1,17 @@
 import React from "react";
-import Dialog from "./Dialog/Dialog";
-import Message from "./Message/Message";
 import style from './Messager.module.css'
 
-let dialogsData = [
-    { id: 1, Name: "Ivan" },
-    { id: 2, Name: "Viktoria" },
-    { id: 3, Name: "Papendus" },
-    { id: 4, Name: "Chubaka" }
-]
 
-let dialogs = dialogsData.map(dialog => 
-<Dialog id={dialog.id} name={dialog.Name} />
-    )
 
-let messagesData = [
-    { id: 1, text: "a;lsmlsmlkmscmck  sdsdsd  scscm" },
-    { id: 2, text: "a;lsmlsmlkmscmck   dcsdc   m" },
-    { id: 3, text: "a;lsmlsmlkmscmckm  sdcsdcsd" },
-    { id: 4, text: "a;lsmlsmlkmscmckmsdcsdsdsdsd" },
-    { id: 5, text: "a;lsmlsmlkmscmckm" }
-]
-
-let messages = messagesData.map( message => <Message id={message.id} text={message.text} />)
-
-let Messager = () => {
+let Messager = (props) => {
     return (
         <div className={style.wrapper}>
             <div className="dialogs">
-                {dialogs}
+                {props.dialogs}
 
             </div>
             <div className="mesages">
-                { messages }
+                { props.messages }
             </div>
         </div>
     )
--- src/index.js
+++ src/index.js
@@ -3,7 +3,30 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Dialog from './components/Messager/Dialog/Dialog';
+import Message from './components/Messager/Message/Message';
 
+
+let dialogsData = [
+  { id: 1, Name: "Ivan" },
+  { id: 2, Name: "Viktoria" },
+  { id: 3, Name: "Papendus" },
+  { id: 4, Name: "Chubaka" }
+]
+
+let dialogs = dialogsData.map(dialog => 
+<Dialog id={dialog.id} name={dialog.Name} />
+  )
+
+let messagesData = [
+  { id: 1, text: "a;lsmlsmlkmscmck  sdsdsd  scscm" },
+  { id: 2, text: "a;lsmlsmlkmscmck   dcsdc   m" },
+  { id: 3, text: "a;lsmlsmlkmscmckm  sdcsdcsd" },
+  { id: 4, text: "a;lsmlsmlkmscmckmsdcsdsdsdsd" },
+  { id: 5, text: "a;lsmlsmlkmscmckm" }
+]
+
+let messages = messagesData.map( message => <Message id={message.id} text={message.text} />)
 
 let postsData = [
   {id: 1, maessage: "sadjnjaiijikcnjjxxjiij", likesCounter: 5},
@@ -14,7 +37,7 @@ let postsData = [
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App postsData={postsData} />
+    <App postsData={postsData} dialogs={dialogs} messages={messages}  />
   </React.StrictMode>
 );
 
