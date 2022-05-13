b76a09272ab4fbccbd17c42b14964edefac75591
Dmitrey1087751 <dmitrey.zadonskey@gmail.com>
2022-05-13 18:24:52 +0200

    why createRef does not work?

--- src/components/Profile/Posts/Posts.jsx
+++ src/components/Profile/Posts/Posts.jsx
@@ -6,14 +6,28 @@ import Post from "./Post/Post";
 
 function Posts(props) {
 
-  let posts = props.postsData.map( post => <Post id={post.id} message={post.maessage} likesCounter={post.likesCounter}/>)
+  let posts = props.postsData.map(post => <Post id={post.id} message={post.maessage} likesCounter={post.likesCounter} />)
+
+
+
+  let newPost = React.useRef();
+
+  let postText = "tratata";
+  // let postText = newPost.current.value;
+  
+
+  let sendPost = () => {
+    alert(postText);
+  }
 
   return (
     <div>
-      <div>FORM</div>
-      { posts }
+      <textarea  ref={newPost} ></textarea>
+      <button onClick={sendPost}>Send</button>
+      {posts}
     </div>
   )
+
 }
 
 export default Posts;
