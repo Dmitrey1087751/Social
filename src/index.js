9e4d32f4f94a4450e0ee98f4f89b6377462b61d6
Dmitrey1087751 <dmitrey.zadonskey@gmail.com>
2022-07-22 16:21:35 +0200

    37 done

--- src/index.js
+++ src/index.js
@@ -1,24 +1,23 @@
-import state, { subscibe } from './redux/state';
+import store from './redux/state';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {addPost} from './redux/state';
-import {updateNewPostText} from './redux/state';
-import { subscribe } from './redux/state';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 let RenderEntireTree = (state) => {
 
 
-root.render(
-  <React.StrictMode>
-    <App state={state} addPost={addPost} updateNewPostText={updateNewPostText} />
-  </React.StrictMode>
-);
+  root.render(
+    <React.StrictMode>
+      <App state={store.getState()}
+        addPost={store.addPost.bind(store)}
+         updateNewPostText={store.updateNewPostText.bind(store)} />
+    </React.StrictMode>
+  );
 }
-RenderEntireTree(state);
+RenderEntireTree(store.getState());
 
-subscribe(RenderEntireTree);
+store.subscribe(RenderEntireTree);
 export default RenderEntireTree
 
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,53 +1,60 @@
-let RenderEntireTree = () => {
-  console.log("OK")
-}
-  let state = {
-      profilePage: {
-          postsData: [
-            { id: 1, message: "sadjnjaiijikcnjjxxjiij", likesCounter: 5 },
-            { id: 2, message: "sadjnjaiiji k cnjj   xxjiij", likesCounter: 4 },
-            { id: 3, message: "вс высфывс sadjnjaiijikcnjjxxjiij", likesCounter: 2 }
-          ],
-          newPostText: "Some text"
-      },
-      messager:{
-          messagesData: [
-            { id: 1, text: "a;lsmlsmlkmscmck  sdsdsd  scscm" },
-            { id: 2, text: "a;lsmlsmlkmscmck   dcsdc   m" },
-            { id: 3, text: "a;lsmlsmlkmscmckm  sdcsdcsd" },
-            { id: 4, text: "a;lsmlsmlkmscmckmsdcsdsdsdsd" },
-            { id: 5, text: "a;lsmlsmlkmscmckm" }
-          ],
-          dialogsData: [
-            { id: 1, Name: "Ivan" },
-            { id: 2, Name: "Viktoria" },
-            { id: 3, Name: "Papendus" },
-            { id: 4, Name: "Chubaka" }
-          ]
-      }
 
+let store = {
+  _state: {
+    profilePage: {
+      postsData: [
+        { id: 1, message: "sadjnjaiijikcnjjxxjiij", likesCounter: 5 },
+        { id: 2, message: "sadjnjaiiji k cnjj   xxjiij", likesCounter: 4 },
+        { id: 3, message: "вс высфывс sadjnjaiijikcnjjxxjiij", likesCounter: 2 }
+      ],
+      newPostText: "Some text"
+    },
+    messager: {
+      messagesData: [
+        { id: 1, text: "a;lsmlsmlkmscmck  sdsdsd  scscm" },
+        { id: 2, text: "a;lsmlsmlkmscmck   dcsdc   m" },
+        { id: 3, text: "a;lsmlsmlkmscmckm  sdcsdcsd" },
+        { id: 4, text: "a;lsmlsmlkmscmckmsdcsdsdsdsd" },
+        { id: 5, text: "a;lsmlsmlkmscmckm" }
+      ],
+      dialogsData: [
+        { id: 1, Name: "Ivan" },
+        { id: 2, Name: "Viktoria" },
+        { id: 3, Name: "Papendus" },
+        { id: 4, Name: "Chubaka" }
+      ]
+    }
+
+  },
+  getState() {
+    return this._state
+  },
+  _renderEntireTree() {
+    console.log("OK")
+  },
+
+  addPost(message) {
+
+    let newPost = {
+      id: 5,
+      message: message,
+      likesCounter: 12
+
+    }
+    this._state.profilePage.postsData.push(newPost);
+    this._renderEntireTree(store._state);
+  },
+  updateNewPostText(message) {
+
+    this._state.profilePage.newPostText = message;
+    this._renderEntireTree(store._state);
+
+  },
+  subscribe(observer) {
+    this._renderEntireTree = observer
   }
- 
-export const addPost = (message) => {
-  
-  let newPost = {
-    id: 5,
-    message: message,
-    likesCounter: 12
-    
-  }
-  state.profilePage.postsData.push(newPost);
-  RenderEntireTree(state);
+
 }
 
-export const updateNewPostText = (message) => {
-  
-  state.profilePage.newPostText = message;
-  RenderEntireTree(state);
-  
-}
 
-export const subscribe = (observer) => {
-RenderEntireTree = observer
-}
-  export default state
+export default store
